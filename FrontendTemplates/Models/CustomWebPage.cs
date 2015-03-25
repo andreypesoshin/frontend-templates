0660398c93600c098c4d8032c9a237881e7ad9de
Andrey Pesoshin <pesoshin@gmail.com>
2015-03-25 17:51:36 +0300

    added a mirroring copy for Products controller (named as ProductsShopTemplates) using ShopTemplates folder

---
+++ FrontendTemplates/Models/CustomWebPage.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using System.Web.WebPages;
+
+namespace FrontendTemplates.Models
+{
+    public abstract class CustomWebPage : WebViewPage
+    {
+
+    }
+
+    public abstract class CustomWebPage<T>: WebViewPage<T>
+    {
+
+    }
+}
