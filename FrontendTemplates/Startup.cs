ddfc9be4fbdfd2f1c63593b1a6907ad2e7a15259
Andrey Pesoshin <pesoshin@gmail.com>
2015-03-26 18:09:23 +0300

    Products controller and authorization removed

--- FrontendTemplates/Startup.cs
+++ FrontendTemplates/Startup.cs
@@ -4,11 +4,10 @@ using Owin;
 [assembly: OwinStartupAttribute(typeof(FrontendTemplates.Startup))]
 namespace FrontendTemplates
 {
-    public partial class Startup
+    public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
         }
     }
 }
