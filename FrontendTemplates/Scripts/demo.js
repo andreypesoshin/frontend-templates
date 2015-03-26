2ab0fffa9f0b393542c4f9f26ea681df29ff2594
Andrey Pesoshin <pesoshin@gmail.com>
2015-03-27 02:13:28 +0300

    some extra code added to Demo page

--- FrontendTemplates/Scripts/demo.js
+++ FrontendTemplates/Scripts/demo.js
@@ -2,11 +2,11 @@ $(function () {
     var headHtml = $("head").html();
 
     var headHtml = headHtml
-        .replace(/^[\s\S]*?<\/title>[\s\S]*?</i, "<") // remove first part of head html content
+        .replace(/^[\s\S]*?<\/title>[\s\S]+?</i, "<") // remove first part of head html content
         .replace(/</g, "&lt;") // escape html tags
         .replace(/>/g, "&gt;")
         .replace(/\n{2,}/g, "\n")
         .replace(/\n/g, "<br/>"); // add line-breaks
 
-    $("blockquote").html(headHtml);
+    $("pre").html(headHtml);
 });
