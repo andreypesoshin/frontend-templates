53655c72888ca9171a4da38731436946c7d72fdb
Andrey Pesoshin <pesoshin@gmail.com>
2015-03-27 11:15:31 +0300

    Extension methods extracted from Models to separate namespace

--- FrontendTemplates/Extensions/ServiceStackBundlerExtensions.cs
+++ FrontendTemplates/Extensions/ServiceStackBundlerExtensions.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using ServiceStack.Html;
 
-namespace FrontendTemplates.Models
+namespace FrontendTemplates.Extensions
 {
     /* 
      * Here we have an option on how to seamlessly avoid using ViewBag.BundleOption
